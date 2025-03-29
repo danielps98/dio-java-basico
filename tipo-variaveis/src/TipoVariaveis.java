8f6b31875400af6a88fe0acb0173437d4093a270
danielpereiradasilva <danielpdsnpk@gmail.com>
2025-03-29 13:29:51 -0300

    exemplos de varias e tipos

--- tipo-variaveis/src/TipoVariaveis.java
+++ tipo-variaveis/src/TipoVariaveis.java
@@ -1,4 +1,5 @@
-public class App {
+public class TipoVariaveis {
+    // Variáveis de instância
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
